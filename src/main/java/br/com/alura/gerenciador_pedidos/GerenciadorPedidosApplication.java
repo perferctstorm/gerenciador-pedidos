21741503a376cb925f78650bdc52f51c98bab36b
Antonio Fagner S. de Oliveira <antonio-fagner.oliveira@serpro.gov.br>
2025-03-28 14:07:15 -0300

    Derived Queries

--- src/main/java/br/com/alura/gerenciador_pedidos/GerenciadorPedidosApplication.java
+++ src/main/java/br/com/alura/gerenciador_pedidos/GerenciadorPedidosApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.gerenciador_pedidos;
 
 import br.com.alura.gerenciador_pedidos.model.Categoria;
-import br.com.alura.gerenciador_pedidos.model.Fornecedor;
-import br.com.alura.gerenciador_pedidos.model.Pedido;
 import br.com.alura.gerenciador_pedidos.model.Produto;
 import br.com.alura.gerenciador_pedidos.repository.CategoriaRepository;
 import br.com.alura.gerenciador_pedidos.repository.FornecedorRepository;
@@ -13,8 +11,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class GerenciadorPedidosApplication implements CommandLineRunner {
@@ -33,6 +31,86 @@ public class GerenciadorPedidosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("Entrando na aplicação.");
+		//var nome = "livro";
+		//1 - Retorne todos os produtos com o nome exato fornecido.
+		//List<Produto> produtos = produtoRepository.findByNomeIgnoreCase(nome);
+		//--List<Produto> produtos = produtoRepository.findByNomeContainingIgnoreCase(nome);
+
+		//2 - Retorne todos os produtos associados a uma categoria específica.
+		//List<Produto> produtos = produtoRepository.findByCategoria_Id(1);
+		//System.out.println(String.format("Lista dos produtos cujo nome são semelhantes a %s:", nome));
+
+		//3 - Retorne produtos com preço maior que o valor fornecido.
+		Double preco = 10.;
+		//List<Produto> produtos = produtoRepository.findByPrecoGreaterThan(preco);
+		//System.out.println(String.format("Lista de produtos acima dos R$ %.2f.", preco));
+
+		//4 - Retorne produtos com preço menor que o valor fornecido.
+		//List<Produto> produtos = produtoRepository.findByPrecoLessThan(preco);
+		//System.out.println(String.format("Produtos com preço abaixo de R$ %.2f: ", preco));
+
+		//5 - Retorne produtos cujo nome contenha o termo especificado.
+		//List<Produto> produtos = produtoRepository.findByNomeContainingIgnoreCase(nome);
+		//System.out.println(String.format("Lista de produtos cujo nome contenha %s: ", nome)) ;
+		//produtos.forEach(p->{
+		//	System.out.println(String.format(" - %s, preço: %.2f.", p.getNome(), p.getPreco()));
+		//});
+
+		//6 - Retorne pedidos que ainda não possuem uma data de entrega.
+		//List<Pedido> pedidos = pedidoRepository.findByDataIsNull();
+		//System.out.println("Lista de pedidos sem data de entrega: ");
+
+		//7 - Retorne pedidos com data de entrega preenchida.
+		//List<Pedido> pedidos = pedidoRepository.findByDataIsNotNull();
+		//System.out.println("Lista de pedidos com data de entrega: ");
+
+		//8 - Retorne produtos de uma categoria ordenados pelo preço.
+		//9 - Retorne produtos de uma categoria ordenados pelo preço de forma decrescente.
+		//10 - Retorne a contagem de produtos em uma categoria específica.
+		Long categoria_id = 2L;
+		Optional<Categoria> categoria = categoriaRepository.findById(categoria_id);
+		if(categoria.isPresent()) {
+			//List<Produto> produtos = produtoRepository.findByCategoria_IdOrderByPreco(categoria_id);
+			//List<Produto> produtos = produtoRepository.findByCategoriaOrderByPrecoDesc(categoria.get());
+			Long totalProdutos = produtoRepository.countByCategoria(categoria.get());
+			System.out.println(String.format("Total de produtos da categoria %s: %d.: ", categoria.get().getNome(), totalProdutos));
+			//produtos.forEach(p -> System.out.println(
+			//		String.format(" - Produto %s, preco: %,3.2f.", p.getNome(),	p.getPreco())));
+		}else{
+			System.out.println(String.format("Categoria %d não encontrada.", categoria_id));
+		}
+
+		System.out.println();
+		Long totalProdutos = produtoRepository.countByPrecoGreaterThan(preco);
+		System.out.println(String.format("Total de produtos cujo preço é maior que %,2.2f: %d.", preco, totalProdutos));
+
+		System.out.println("");
+		System.out.println("Os 3 produtos mais caros: ");
+		List<Produto> produtos = produtoRepository.findTop2ByOrderByPrecoDesc();
+		produtos.forEach(p -> System.out.println(
+				String.format(" - Produto %s, preco: %,3.2f.", p.getNome(),	p.getPreco())));
+
+		System.out.println("");
+		System.out.println("Os 3 produtos mais baratos: ");
+		produtos = produtoRepository.findTop2ByOrderByPreco();
+		produtos.forEach(p -> System.out.println(
+				String.format(" - Produto %s, preco: %,3.2f.", p.getNome(),	p.getPreco())));
+
+
+		System.out.println();
+		if(categoria.isPresent()) {
+			produtos = produtoRepository.findTop1ByCategoriaOrderByPreco(categoria.get());
+			System.out.println(String.format("Produtos mais baratos da categoria %s: ", categoria.get().getNome()));
+			produtos.forEach(p -> System.out.println(
+					String.format(" - Produto %s, preco: %,3.2f.", p.getNome(),	p.getPreco())));
+		}else{
+			System.out.println(String.format("Categoria %d não encontrada.", categoria_id));
+		}
+		System.out.println("\nSaindo na aplicação.");
+
+
+/*
 		// Criando categorias
 		Categoria categoriaEletronicos = new Categoria(1L, "Eletrônicos");
 		Categoria categoriaLivros = new Categoria(2L, "Livros");
@@ -71,5 +149,6 @@ public class GerenciadorPedidosApplication implements CommandLineRunner {
 			System.out.println(String.format("Produto: %s, Fornecedor: %s.",
 					p.getNome(), p.getFornecedor().getNome()));
 		});
+*/
 	}
 }
--- src/main/java/br/com/alura/gerenciador_pedidos/repository/ProdutoRepository.java
+++ src/main/java/br/com/alura/gerenciador_pedidos/repository/ProdutoRepository.java
@@ -1,8 +1,22 @@
 package br.com.alura.gerenciador_pedidos.repository;
 
+import br.com.alura.gerenciador_pedidos.model.Categoria;
 import br.com.alura.gerenciador_pedidos.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+import java.util.List;
 
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findByNomeIgnoreCase(String nome);
+    List<Produto> findByCategoria_Id(Integer id);
+    List<Produto> findByPrecoGreaterThan(Double preco);
+    List<Produto> findByPrecoLessThan(Double preco);
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
+    List<Produto> findByCategoria_IdOrderByPreco(Long id);
+    List<Produto> findByCategoriaOrderByPrecoDesc(Categoria categoria);
+    Long countByCategoria(Categoria categoria);
+    Long countByPrecoGreaterThan(Double preco);
+    List<Produto> findTop2ByOrderByPrecoDesc();
+    List<Produto> findTop2ByOrderByPreco();
+    List<Produto> findTop1ByCategoriaOrderByPreco(Categoria categoria);
 }
